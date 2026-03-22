6876a19254f8f849dadc1402b3aa51312c43ccbe
Dinh Thien Tu Tran <kevinthientran2323@gmail.com>
2026-03-22 14:38:53 -0700

    Add Tasks

---
+++ routes/tasks.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const Task = require('../models/Task');
+
+const router = express.Router();
+
+//GET tasks
+router.get('/', async(req, res) => {
+    try 
+    {
+        const rasks = (await Task.find()).toSorted({createAt: -1});
+        res.status(200).json(tasks);
+    } catch (error) 
+    {
+        res.status(500).json({ messae:'Failed to fetch tasks'});
+    }        
+});
+
+//POST create a new task
+router.post('/', async (req, res) => {
+    try 
+    {
+        const { title } = req.body;
+
+        if (!title || !title.trim()) 
+        {
+            return res.status(400).json({message: 'Title is reqired'});
+        }
+
+        const newTask = await Task.create({
+            title: title.trim(),
+        });
+        res.status(201).json(newTask);
+    } catch (error)
+    {
+        res.status(500).json({message: 'Failed to create task'});
+    }
+});
+
+//PUT update task
+router.put('/:id', async (req, res) => {
+    try
+    {
+        const {title, completed} = req.body;
+
+        const updatedTask = await Task.findByIdAndUpdate (
+            req.params.id,
+            {title, completed},
+            {new: true, runValidators: true}
+        );
+
+        if (!updatedTask) {
+            return res.status(404).json({ message: 'Task not found'});
+        }
+
+        res.status(200).json(updatedTask);
+    } catch
+    {
+        res.status(500).json({message: 'Failed to update task'});
+    }
+});
+
+//DELETE task
+router.delete('/:id', async (req, res) => {
+    try
+    {
+        const deleteTask = await Task.findByIdAndDelete(req.params.id);
+
+        if(!deleteTask)
+        {
+            return res.status(400).json({message: ' Task not found'});
+        }
+
+        res.status(200).json({message: 'Task deleted successfully'});
+    }
+    catch
+    {
+        res.status(500).json({message: 'Failed to delete task'});
+    }
+});
+
+module.exports = router;
